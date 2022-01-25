7b894b93985cbcc7e5be7974e286496af7874f8b
s1646477 <s1646477@baltic10.geos.ed.ac.uk>
2022-01-25 23:59:19 +0000

    Tried writing sorting algorithm, got pretty stuck. Looked up other sorts, like bubble sort, but I don't use minimum value like you asked.

--- test3.py
+++ test3.py
@@ -1,8 +1,33 @@
 import numpy as np
-import sys
 
 def randomarray(n):
-    data=np.random.random(n)
+    data=np.random.random((n))
     print(data)
 
-randomarray(5)
+def minval():
+    x=min(np.random.random((10)))
+    
+
+def bubblesort(n):
+    data=np.random.random((n))
+    for i in range (1,n):
+        for j in range (0,n-1):
+            if data[j]>data[j+1]:
+                temp = data[j]
+                data[j]=data[j+1]
+                data[j+1]=temp
+    print(data)
+
+def sort():
+    data=np.random.random((5))
+    print(data)
+    sortdata=np.empty((5))
+    x=min(data)
+    while len(data)>0:
+        sortdata.append('x')
+        data.remove('x')
+    print(sortdata)
+
+sort()
+
+
