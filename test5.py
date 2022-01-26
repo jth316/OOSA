794bbda0580cb87d53c1175adffe456a403449fc
s1646477 <s1646477@baltic10.geos.ed.ac.uk>
2022-01-26 07:25:49 +0000

    Made sorting algorithm a function

--- test5.py
+++ test5.py
@@ -1,17 +1,18 @@
 import numpy as np
 
-data=np.random.random((5))
-sortdata=[]
-print(data)
-while len(data)>0:
-    x=data[0]
-    for i in range (1,len(data)):
-        if data[i]<x:
-            x=data[i]
-    print(x)
-    sortdata.append(x)
-    data = np.delete(data, np.where(data == x))
-print(sortdata)
-print(data)
+def minValue(n):
+    data=np.random.random((n))
+    sortdata=[]
+    print(data)
+    while len(data)>0:
+        x=data[0]
+        for i in range (1,len(data)):
+            if data[i]<x:
+                x=data[i]
+        print(x)
+        sortdata.append(x)
+        data = np.delete(data, np.where(data == x))
+    print(sortdata)
+    print(data)
 
-
+minValue(5)
